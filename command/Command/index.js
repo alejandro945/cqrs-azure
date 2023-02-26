450bb59a2f7601de1b445631463d6ca11208b754
alejandroTTT <lejandro@twototango.co>
2023-02-26 03:45:10 +0000

    Fix: Res Problem var not defined :bug:

--- command/Command/index.js
+++ command/Command/index.js
@@ -3,23 +3,23 @@ const sql = require('mssql');
 require('dotenv').config();
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    context.log(process.env["DB_USER"]);
-    try {
-      // Getting database and container
-      const pool = await sql.connect({
-        server: process.env["DB_SERVER"],
-        database: process.env["DB_DATABASE"],
-        user: process.env["DB_USER"],
-        password: process.env["DB_PASSWORD"],
-        encrypt: true
-      });
-  
-      // Creating person in write database
-      await pool.request().query`INSERT INTO Persons (ID, FirstName, Age) VALUES (${req.body.id}, ${req.body.name}, ${req.body.age})`;
-      res.status(201).json({ mensaje: 'Registro creado correctamente' });;
-    } catch (error) {
-      context.log(error);
-      res.status(500).json({ error: 'Error creating person' });
-    }
+  context.log('JavaScript HTTP trigger function processed a request.');
+  context.log(process.env["DB_USER"]);
+  try {
+    // Getting database and container
+    const pool = await sql.connect({
+      server: process.env["DB_SERVER"],
+      database: process.env["DB_DATABASE"],
+      user: process.env["DB_USER"],
+      password: process.env["DB_PASSWORD"],
+      encrypt: true
+    });
+
+    // Creating person in write database
+    await pool.request().query`INSERT INTO Persons (ID, FirstName, Age) VALUES (${req.body.id}, ${req.body.name}, ${req.body.age})`;
+    context.res = { status: 201, body: 'Registro creado correctamente' };
+  } catch (error) {
+    context.log(error);
+    context.res = { status: 500, body: error.message };
+  }
 }
